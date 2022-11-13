ffaade4e22ac40df25debc8f40f7064685bdeced
Gonda Gábor <energyteamgazuru@gmail.com>
2022-11-13 21:46:32 +0100

    Update BaseEntity ID field

--- spring-boot-server/src/main/java/hu/bme/hit/teamperec/data/entity/BaseEntity.java
+++ spring-boot-server/src/main/java/hu/bme/hit/teamperec/data/entity/BaseEntity.java
@@ -1,7 +1,10 @@
 package hu.bme.hit.teamperec.data.entity;
 
 import java.util.UUID;
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +17,7 @@ import org.hibernate.annotations.Type;
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Type(type = "uuid-char")
     private UUID id;
 
