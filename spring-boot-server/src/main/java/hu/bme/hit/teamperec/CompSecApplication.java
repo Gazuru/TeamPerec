ee6ef6fb08832c0f9507302970246402733f8201
Gonda Gábor <energyteamgazuru@gmail.com>
2022-11-13 11:38:53 +0100

    Update dependencies, remove deprecated method usages

--- spring-boot-server/src/main/java/hu/bme/hit/teamperec/CompSecApplication.java
+++ spring-boot-server/src/main/java/hu/bme/hit/teamperec/CompSecApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityJwtApplication {
+public class CompSecApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+        SpringApplication.run(CompSecApplication.class, args);
     }
 
 }
