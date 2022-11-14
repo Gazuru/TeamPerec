4749ff02b7f60d6a250bc0d268f4b696d5f078ee
Albert Fazekas <khhun12@gmail.com>
2022-11-14 10:40:18 +0100

    Frontend base and pom changes(jwt dependencies)

---
+++ frontend/src/app/components/details-caff/details-caff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-caff',
+  templateUrl: './details-caff.component.html',
+  styleUrls: ['./details-caff.component.css']
+})
+export class DetailsCaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
