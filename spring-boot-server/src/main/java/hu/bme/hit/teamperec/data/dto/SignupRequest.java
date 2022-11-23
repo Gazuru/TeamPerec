0b1d701f540f3bd371dbba5fc72d545e35c28a9b
Gonda Gábor <energyteamgazuru@gmail.com>
2022-11-23 17:27:16 +0100

    Update javax.validation to jakarta.validation

--- spring-boot-server/src/main/java/hu/bme/hit/teamperec/data/dto/SignupRequest.java
+++ spring-boot-server/src/main/java/hu/bme/hit/teamperec/data/dto/SignupRequest.java
@@ -1,10 +1,10 @@
 package hu.bme.hit.teamperec.data.dto;
 
 import java.util.Set;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
