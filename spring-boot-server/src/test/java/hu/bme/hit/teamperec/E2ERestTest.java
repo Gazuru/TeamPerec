fecd6a49f0a8bf9f9dd13748d7766c7948a55d58
Gonda Gábor <energyteamgazuru@gmail.com>
2022-12-03 17:53:01 +0100

    Comment posting, user get test

--- spring-boot-server/src/test/java/hu/bme/hit/teamperec/E2ERestTest.java
+++ spring-boot-server/src/test/java/hu/bme/hit/teamperec/E2ERestTest.java
@@ -8,14 +8,17 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.bme.hit.teamperec.data.dto.CAFFDto;
+import hu.bme.hit.teamperec.data.dto.CommentDto;
 import hu.bme.hit.teamperec.data.dto.LoginRequest;
+import hu.bme.hit.teamperec.data.entity.User;
+import hu.bme.hit.teamperec.data.enums.ERole;
 import hu.bme.hit.teamperec.data.repository.RoleRepository;
-import hu.bme.hit.teamperec.data.response.CAFFDownloadResponse;
-import hu.bme.hit.teamperec.data.response.CAFFResponse;
-import hu.bme.hit.teamperec.data.response.JwtResponse;
+import hu.bme.hit.teamperec.data.repository.UserRepository;
+import hu.bme.hit.teamperec.data.response.*;
 import hu.bme.hit.teamperec.service.CAFFService;
 import hu.bme.hit.teamperec.service.CommentService;
 import hu.bme.hit.teamperec.service.UserService;
@@ -34,6 +37,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -60,6 +64,9 @@ class E2ERestTest {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private Header authHeader;
 
     private byte[] caffFile1;
@@ -67,6 +74,9 @@ class E2ERestTest {
     private byte[] caffFile2;
 
     private byte[] caffFile3;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @BeforeAll
     public void setup() throws JSONException {
@@ -226,7 +236,7 @@ class E2ERestTest {
     void shouldDeleteCaff() {
         var test = "shouldDeleteCaff";
         var caff1 = createCaff(test + "_caff1", test + "_desc1", caffFile1);
-        createCaff(test + "_caff2", test + "_desc2", caffFile2);
+        var caff2 = createCaff(test + "_caff2", test + "_desc2", caffFile2);
 
         var getResponses = RestAssured.given()
                 .header(authHeader)
@@ -245,14 +255,139 @@ class E2ERestTest {
                 .header(authHeader)
                 .contentType(ContentType.JSON)
                 .delete("http://localhost:" + port + "/api/caff/" + caff1.id())
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
 
-        getResponses
+        getResponses = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/caff/list")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .body()
+                .jsonPath()
+                .getList(".", CAFFResponse.class);
 
-        assertThat(responseId).isEqualTo(caff1.id());
+        var getResponseIds = getResponses.stream().map(CAFFResponse::id).toList();
+
+        assertThat(getResponseIds).hasSize(1).containsExactly(caff2.id());
     }
 
+    @Test
+    void shouldPostComment() {
+        var test = "shouldPostComment";
+        var caff1 = createCaff(test + "_caff1", test + "_desc1", caffFile1);
+        var comment = new CommentDto(test + "_comment");
 
-    private String login(String username, String password) throws JSONException {
+        var commentResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .body(comment)
+                .post("http://localhost:" + port + "/api/caff/" + caff1.id() + "/comment")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .as(CommentResponse.class);
+
+        var caffResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/caff/" + caff1.id())
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(CAFFResponse.class);
+
+        var commentsOfCaff = caffResponse.comments().stream().map(CommentResponse::id).toList();
+
+        assertThat(commentResponse.commentText()).isEqualTo(comment.commentText());
+        assertThat(commentsOfCaff).containsExactly(commentResponse.id());
+    }
+
+    @Test
+    void shouldDeleteComment() {
+        var test = "shouldDeleteComment";
+        var caff1 = createCaff(test + "_caff1", test + "_desc1", caffFile1);
+        var comment = new CommentDto(test + "_comment");
+
+        var commentResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .body(comment)
+                .post("http://localhost:" + port + "/api/caff/" + caff1.id() + "/comment")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .as(CommentResponse.class);
+
+        var caffResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/caff/" + caff1.id())
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(CAFFResponse.class);
+
+        assertThat(caffResponse.comments()).hasSize(1);
+
+        RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .delete("http://localhost:" + port + "/api/comment/" + commentResponse.id())
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        caffResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/caff/" + caff1.id())
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(CAFFResponse.class);
+
+        assertThat(caffResponse.comments()).isEmpty();
+    }
+
+    @Test
+    void shouldListUsers() {
+        var newUser = createUser("user", "user", "user@email.em");
+
+        var userResponses = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/user/list")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .jsonPath()
+                .getList(".", UserResponse.class);
+
+        var userNames = userResponses.stream().map(UserResponse::name).toList();
+
+        assertThat(userNames).hasSize(2).containsExactly("admin", newUser.getUsername());
+    }
+
+    @Test
+    void shouldGetUser() {
+        var currentUserId = userService.getUserByUsername("admin").getId();
+
+        var userResponse = RestAssured.given()
+                .header(authHeader)
+                .contentType(ContentType.JSON)
+                .get("http://localhost:" + port + "/api/user/" + currentUserId)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(UserResponse.class);
+
+        assertThat(userResponse.email()).isEqualTo("admin@admin.admin");
+        assertThat(userResponse.name()).isEqualTo("admin");
+    }
+
+    private String login(String username, String password) {
         var loginRequest = new LoginRequest(username, password);
 
         var response = RestAssured.given()
@@ -265,6 +400,16 @@ class E2ERestTest {
                 .as(JwtResponse.class);
 
         return response.getToken();
+    }
+
+    private User createUser(String username, String password, String email) {
+        var user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        var userRole = roleRepository.findByName(ERole.ROLE_USER).get();
+        user.setRoles(Set.of(userRole));
+        return userRepository.save(user);
     }
 
     private byte[] readFileToBytes(String path) {
